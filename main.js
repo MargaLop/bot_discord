8b61f45d848326647e358d3abd2fe244c4c396a5
Marga <margalppalacios@gmail.com>
2022-07-12 12:19:55 +0200

    ping pong

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./cofig.json');
 
+const prefix = "-";
+
 
 const client = new Client({ 
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] 
 });
 
 client.once('ready', (bot) => {
+  console.log('********************************************************')
 	console.log(`BOT: ${bot.user.username}\nStatus:${bot.presence.status}`);
+  console.log('********************************************************')
 });
 
-client.on("messageCreate", (msg) =>{
-  if(msg.author.bot) 
-   return console.log(`Mensaje del Bot ${msg.author.username}`);
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
 
-  // console.log(msg)
-  msg.reply('Buenos dias bella persona!')
+  if (!msg.content.startsWith(prefix))
+    return msg.reply("Buenos dias bella personas!");
+
+  if (msg.content.startsWith(prefix)) {
+    const argumentos = msg.content.slice(prefix.length).split(/ +/);
+    const comando = argumentos.shift().toLowerCase();
+
+    if (comando == "ping") return msg.reply("pong");
+  }
 });
-
 client.login(token);
