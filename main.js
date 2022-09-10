e632ca8275c390091cdf0e31f7fa96a840faa5ae
Marga <margalppalacios@gmail.com>
2022-09-10 13:29:52 +0200

    eliminar otros comandos

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js'); 
-const { token, apitoken } = require("./cofig.json"); 
-const axios = require("axios") 
- 
-const prefix = "-"; 
+const Discord = require('discord.js');
+const { token, apitoken } = require("./cofig.json");
+const axios = require("axios");
+
+const prefix = "-";
 
 
-const client = new Discord.Client({ intents: [ 
-  Discord.Intents.FLAGS.GUILDS, 
-  Discord.Intents.FLAGS.GUILD_MESSAGES, 
+const client = new Discord.Client({ intents: [
+  Discord.Intents.FLAGS.GUILDS,
+  Discord.Intents.FLAGS.GUILD_MESSAGES,
 ]});
 
 client.once('ready', (bot) => {
@@ -16,82 +16,74 @@ client.once('ready', (bot) => {
   console.log('********************************************************')
 });
 
-const exampleEmbed = ( 
-  temp, 
-  maxTemp, 
-  minTemp, 
-  pressure, 
-  humidity, 
-  wind, 
-  cloudness, 
-  icon, 
-  author, 
-  profile, 
-  cityName, 
-  country 
-) => 
-new Discord.MessageEmbed() 
-  .setColor('#0099ff') 
-  .setAuthor(`Hello, ${author}`, profile) 
-  .setTitle(`There is ${temp}\u00B0 C in ${cityName}, ${country}`) 
-  .addField(`Maximum Temperature:`, `${maxTemp}\u00B0 C`, true) 
-  .addField(`Minimum Temperature:`, `${minTemp}\u00B0 C`, true) 
-  .addField(`Humidity:`, `${humidity} %`, true) 
-  .addField(`Wind Speed:`, `${wind} m/s`, true) 
-  .addField(`Pressure:`, `${pressure} hpa`, true) 
-  .addField(`Cloudiness:`, `${cloudness}`, true) 
-  .setThumbnail(`http://openweathermap.org/img/w/${icon}.png`); 
- 
+const embed = Object.freeze({
+exampleEmbed:(
+  temp,
+  maxTemp,
+  minTemp,
+  pressure,
+  humidity,
+  wind,
+  cloudness,
+  icon,
+  author,
+  cityName,
+  country
+) =>
+new Discord.MessageEmbed()
+  .setColor('#6DC4FF')
+  .setAuthor(`Hola, ${author}`, profile)
+  .setTitle(`Hay ${temp}\u00B0 C en ${cityName}, ${country}`)
+  .addField(`🌡 Maximas Temperaturas:`, `${maxTemp}\u00B0 Cº`, true)
+  .addField(`🌡 Minimas Temperaturas:`, `${minTemp}\u00B0 Cº`, true)
+  .addField(`💧 Humedad:`, `${humidity} %`, true)
+  .addField(`💨 Viento:`, `${wind} m/s`, true)
+  .addField(`📊 Precipitaciones:`, `${pressure} hpa`, true)
+  .addField(`⛅️ Nubes:`, `${cloudness}`, true) 
+  .setUrl(`http://openweathermap.org/img/w/${icon}.png`)
+});
 
 client.on("messageCreate", (msg) => {
   if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix))
-    return msg.reply("Buenos dias bella personas!");
-
+  
   if (msg.content.startsWith(prefix)) {
     const argumentos = msg.content.slice(prefix.length).split(/ +/);
     const comando = argumentos.shift().toLowerCase();
     const argWeather = argumentos[0]
 
-    
-    if (comando == "ping") return msg.reply("pong");
 
-    if (comando == "suma") {
-      return msg.reply(
-        `Su suma da: ${
-          parseFloat(argumentos[0]) + parseFloat(argumentos[1])
-        }`
-    )}
-
-    if (comando == "beep")  return msg.reply("Beep-Boop!") 
-
-    if (comando  == "server")  
-      return msg.reply("Nombre del servidor : " + msg.guild.name + "\nNúmero de miembros: " + msg.guild.memberCount); 
-
-    if (comando  == "user")  
-      return msg.reply("Tu usuario: " + msg.author.username + "\nTu ID: " + msg.author.id);
-    
-    if (comando == "w" || "weather") { 
+    if (comando == "w" || "weather") {
       axios 
-          .get(`https://api.openweathermap.org/data/2.5/weather?q=${argWeather}&units=metric&appid=${apitoken}`) 
-          .then(response => { 
-              let apiData = response; 
-              let currentTemp = Math.ceil(apiData.data.main.temp) 
-              let maxTemp = apiData.data.main.temp_max; 
-              let minTemp = apiData.data.main.temp_min; 
-              let humidity = apiData.data.main.humidity; 
-              let wind = apiData.data.wind.speed; 
-              let author = msg.author.username 
-              let profile = msg.author.displayAvatarURL 
-              let icon = apiData.data.weather[0].icon 
-              let cityName = argWeather
-              let country = apiData.data.sys.country 
-              let pressure = apiData.data.main.pressure; 
-              let cloudness = apiData.data.weather[0].description; 
-              msg.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country)); 
-          }).catch(err => { 
-              msg.reply(`Enter a vailid city name`)
-          }) 
+          .get(`https://api.openweathermap.org/data/2.5/weather?q=${argWeather}&units=metric&appid=${apitoken}`)
+          .then(response => {
+              let apiData = response;
+              let currentTemp = Math.ceil(apiData.data.main.temp);
+              let maxTemp = apiData.data.main.temp_max;
+              let minTemp = apiData.data.main.temp_min;
+              let humidity = apiData.data.main.humidity;
+              let wind = apiData.data.wind.speed;
+              let author = msg.author.username;
+              let icon = apiData.data.weather[0].icon;
+              let cityName = argWeather;
+              let country = apiData.data.sys.country;
+              let pressure = apiData.data.main.pressure;
+              let cloudness = apiData.data.weather[0].description;
+
+              console.log(currentTemp)
+              console.log(maxTemp)
+              console.log(minTemp)
+              console.log(humidity)
+              console.log(wind)
+              console.log(author)
+              console.log(icon)
+              console.log(cityName)
+              console.log(country )
+              console.log(pressure)
+              console.log(cloudness)
+              msg.reply(embed.exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
+            }).catch(err => {
+              msg.reply("Introduce una ciudad validad")
+          })
     }
   }
 });
